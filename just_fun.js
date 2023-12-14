eec977d9a932c90db8e1249e5f3913feb75b4e90
KRUNAL JOGI <69499961+jogi-bot@users.noreply.github.com>
2023-12-14 13:41:46 +0530

    add new code

--- just_fun.js
+++ just_fun.js
@@ -88,3 +88,102 @@ main();
 // //output : -
 // //kem bapu
 // //majama
+
+
+function Travel(arr1) {
+
+return   arr1.reduce((acc , itr) => {
+     return acc + itr
+ },0)
+
+ 
+}
+const arr1 = [1, 2, 3, 4, 5];
+console.log(Travel(arr1))
+
+// //15
+
+
+const Nemo = new  Array(11).fill('nemo')
+
+function findNemo(array) {
+  
+  for (let i = 0; i < array.length; i++){
+    console.log(`loop running`);
+    if (array[i] === 'nemo') {
+      console.log('we found-nemo');
+      break
+      }
+  }
+ 
+}
+findNemo(Nemo)
+
+//loop running
+// we found-nemo
+
+const arr1 = ['a', 'b', 'c', 'd']
+
+function krunal(arr1) {
+    for (let i = 0; i < arr1.length; i++){
+        for (let j = 0; j < arr1.length; j++) {
+            console.log(arr1[i],arr1[j]);
+        }
+    }
+
+}
+
+krunal(arr1)
+
+// a a
+// a b
+// a c
+// a d
+// b a
+// b b
+// b c
+// b d
+// c a
+// c b
+// c c
+// c d
+// d a
+// d b
+// d c
+// d d
+
+arr1 = [1,2,3,4]
+function printNUmbersWithSum(arr1) {
+    arr1.forEach(element => {
+        console.log(`these all are numbers ${ element }`);
+    });
+
+    arr1.forEach(function (first) {
+        arr1.forEach(function (second) {
+             console.log(first + second);
+        })
+    })
+    
+}
+printNUmbersWithSum(arr1)
+
+// these all are numbers 1
+// these all are numbers 2
+// these all are numbers 3
+// these all are numbers 4
+// 2
+// 3
+// 4
+// 5
+// 3
+// 4
+// 5
+// 6
+// 4
+// 5
+// 6
+// 7
+// 5
+// 6
+// 7
+// 8
